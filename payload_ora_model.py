d18282d49a5cf6e1088e834b89d4a3dc950760bd
RaghuPythonProjects <118695122+RaghuPythonProjects@users.noreply.github.com>
2024-06-04 22:12:36 +0530

    Update payload_ora_model.py

--- payload_ora_model.py
+++ payload_ora_model.py
@@ -1,15 +1,14 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, func, Identity
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class payload_ora_model(Base):
+class PayloadOraModel(Base):
     __tablename__ = 'payload'
 
-    _id = Column(Integer, primary_key=True)  # TODO: make it identity column
-    timestamp = Column(DateTime, nullable=False)  # TODO: set to auto populate current time
+    _id = Column(Integer, Identity(start=1, cycle=True), primary_key=True)
+    timestamp = Column(DateTime, default=func.now(), nullable=False)
     payload_received = Column(String, nullable=False)
 
     def __repr__(self):
         return f"<Payload(_id={self._id}, payload_received='{self.payload_received}', timestamp='{self.timestamp}')>"
-    
